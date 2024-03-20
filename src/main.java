86e52b665043907453be7f2e03e42007c8307005
DeveloperUchida <kouei28poiou@icloud.com>
2024-03-20 12:38:25 +0900

    DataUpdate 2024/03/20

--- src/main.java
+++ src/main.java
@@ -4,7 +4,7 @@ public class main {
     private static final String USERNAME = "user";
     private static final String PASSWORD = "password";
 
-    public static void main(string[] args){
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("ログインしてください");
@@ -17,6 +17,16 @@ public class main {
         System.out.print("パスワード: ");
         String inputPassword = scanner.nextLine();
 
-
+        //入力されたユーザー名とパスワードが正しいかどうかをチェック
+        if(login(inputUsername, inputPassword)){
+            System.out.println("ログイン成功！！");
+            //ログイン後の処理を下記に記載
+        }else{
+            System.out.println("ログインに失敗しました。ユーザー名やパスワードを確認の上もう一度入力してください。");
+        }
+    }
+    //ログイン処理
+    private static boolean login(String username, String passowrd){
+        return username.equals(USERNAME) && passowrd.equals(PASSWORD);
     }
 }
