eb5ae2ce6adbb57d441386e78e57943078c5018c
DeveloperUchida <kouei28poiou@icloud.com>
2024-03-20 11:56:06 +0900

    DataUpdate 2024/03/20

--- src/main.java
+++ src/main.java
@@ -1,5 +1,22 @@
 import java.util.Scanner;
 public class main {
     //バーコードされたユーザー名とパスワード
+    private static final String USERNAME = "user";
+    private static final String PASSWORD = "password";
 
+    public static void main(string[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("ログインしてください");
+
+        //ユーザー名の入力
+        System.out.print("ユーザー名: ");
+        String inputUsername = scanner.nextLine();
+
+        //パスワード入力
+        System.out.print("パスワード: ");
+        String inputPassword = scanner.nextLine();
+
+
+    }
 }
